0f8fda714412bff57be2958440aff320c6fa1e39
siska199 <siska.apriana.1703226@students.um.ac.id>
2022-12-13 18:45:12 +0700

    add useOnChange

--- components/form/personal-info.tsx
+++ components/form/personal-info.tsx
@@ -3,69 +3,15 @@ import InputPhone from '@atoms/input/phone'
 import TextArea from '@atoms/input/text-area'
 import UploadPhoto from '@atoms/input/upload-photo'
 import ListSocialMedia from '@components/list/social-media'
-import { generateURLImage } from '@lib/helper'
 import { handleChangeField } from '@lib/redux/features/resumeSlice'
 import { useAppDispatch, useAppSelector } from '@lib/redux/store'
+import useOnChange from 'hooks/useOnChange'
 import React, { useState } from 'react'
 
 const FormPersonalInfo = (props : {}) => {
   const dispatch = useAppDispatch()
   const form = useAppSelector(state=>state.resume.form.personalInfo) 
-  const [file, setFile] = useState<File | null>(null)
-
-  const handleOnChange = (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)=>{
-    e.preventDefault()
-    const data = {
-      type : "personalInfo",
-      value : e.target.value,
-      name : e.target.name
-    }
-    dispatch(handleChangeField(data))
-  }
-
-  const handleOnChangePhone = (dataInput : TDataInputCustome)=>{
-    const data = {
-      type : "personalInfo",
-      value : dataInput.value,
-      name : dataInput.name
-    }
-    dispatch(handleChangeField(data))
-  }
-
-  const handleOnChangeImage = (e:React.ChangeEvent<HTMLInputElement>)=>{
-    const target = e.target
-    if(target.files){
-      //Generate URL :
-      const imageName = target.files[0]? target.files[0].name : ""
-        const callback =(url:TFileReader)=>{
-          const data = {
-          type : "personalInfo",
-          value : {
-            file : null,
-            url : url,
-            imageName 
-          },
-            name : target.name
-          }
-          dispatch(handleChangeField(data))
-        }
-        generateURLImage(target.files[0], callback)
-    }
-  }
-
-  const handleRemoveImage = ()=>{
-    const data = {
-      type : "personalInfo",
-      value : {
-        file : null,
-        url : "",
-        imageName : ""
-      },
-      name : "image"
-    }
-    dispatch(handleChangeField(data))
-  }
-
+  const  { handleOnChange, handleOnChangeImage,handleOnChangePhone,handleRemoveImage} = useOnChange("personalInfo")
 
   return (
     <div className='container-form'>
---
+++ hooks/useOnChange.tsx
@@ -0,0 +1,67 @@
+import { generateURLImage } from '@lib/helper'
+import { handleChangeField } from '@lib/redux/features/resumeSlice'
+import { useAppDispatch } from '@lib/redux/store'
+import React from 'react'
+
+type TProps = {
+    typeForm : string;
+}
+
+const useOnChange = (typeForm:string ) => {
+   const dispatch = useAppDispatch()
+   
+   const handleOnChange = (e : React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>)=>{
+    e.preventDefault()
+    const data = {
+      type : typeForm,
+      value : e.target.value,
+      name : e.target.name
+    }
+    dispatch(handleChangeField(data))
+  }
+
+  const handleOnChangePhone = (dataInput : TDataInputCustome)=>{
+    const data = {
+      type :  typeForm,
+      value : dataInput.value,
+      name : dataInput.name
+    }
+    dispatch(handleChangeField(data))
+  }
+
+  const handleOnChangeImage = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    const target = e.target
+    if(target.files){
+      const imageName = target.files[0]? target.files[0].name : ""
+        const callback =(url:TFileReader)=>{
+          const data = {
+          type :  typeForm,
+          value : {
+            file : null,
+            url : url,
+            imageName 
+          },
+            name : target.name
+          }
+          dispatch(handleChangeField(data))
+        }
+        generateURLImage(target.files[0], callback)
+    }
+  }
+  const handleRemoveImage = ()=>{
+    const data = {
+      type : typeForm,
+      value : {
+        file : null,
+        url : "",
+        imageName : ""
+      },
+      name : "image"
+    }
+    dispatch(handleChangeField(data))
+  }
+  
+  return { handleOnChange, handleOnChangeImage,handleRemoveImage,handleOnChangePhone}
+}
+
+export default useOnChange
